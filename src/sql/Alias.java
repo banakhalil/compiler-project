fa36f6be8a4811fbeb02f08f5f29707b502ccb7d
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-18 14:11:20 +0300

    added alias class

---
+++ src/sql/Alias.java
@@ -0,0 +1,31 @@
+package sql;
+
+public class Alias extends ASTNode {
+    private Identifier identifier;
+    private String stringValue;
+    private boolean isString;
+    
+    public Alias(Identifier identifier) {
+        this.identifier = identifier;
+        this.isString = false;
+    }
+    
+    public Alias(String stringValue) {
+        this.stringValue = stringValue;
+        this.isString = true;
+    }
+    
+    public boolean isString() {
+        return isString;
+    }
+
+    
+    @Override
+    public String prettyPrint(String indent) {
+        if (isString) {
+            return indent + "Alias (string): \"" + stringValue + "\"";
+        } else {
+            return indent + "Alias:\n" + identifier.prettyPrint(indent + "  ");
+        }
+    }
+}
