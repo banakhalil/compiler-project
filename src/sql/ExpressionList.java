f4e1d78d9146216a7637d48b358c532b24b929c2
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-20 12:12:33 +0300

    added expression list class

---
+++ src/sql/ExpressionList.java
@@ -0,0 +1,20 @@
+package sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionList extends ASTNode{
+    private List<ASTNode> children = new ArrayList<>();
+    public ExpressionList(List<ASTNode> children) {
+        this.children = children;
+    }
+    @Override
+    public String prettyPrint(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent).append("EXPRESSION LIST:\n");
+        for (ASTNode child : children) {
+            sb.append(child.prettyPrint(indent + "  ")).append("\n");
+        }
+        return sb.toString().trim();
+    }
+}
