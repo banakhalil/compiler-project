06b7bff23035b34906ac31894114dd53056c548d
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-20 12:34:28 +0300

    added expression class

---
+++ src/sql/Expression.java
@@ -0,0 +1,36 @@
+package sql;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Expression extends ASTNode{
+    private ASTNode primaryExpression;
+    private ASTNode expressionList;
+    private ASTNode selectStatment;
+    private ASTNode exitPredicate;
+    private ASTNode scalarSubquery;
+    private List<ASTNode> whenClauseChildren = new ArrayList<>();
+
+    public Expression(ASTNode primaryExpression, ASTNode expressionList, ASTNode selectStatment, ASTNode exitPredicate, ASTNode scalarSubquery, List<ASTNode> whenClauseChildren) {
+        this.primaryExpression = primaryExpression;
+        this.expressionList = expressionList;
+        this.selectStatment = selectStatment;
+        this.exitPredicate = exitPredicate;
+        this.scalarSubquery = scalarSubquery;
+        this.whenClauseChildren = whenClauseChildren;
+    }
+    @Override
+    public String prettyPrint(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent).append("EXPRESSION:\n");
+        sb.append(primaryExpression.prettyPrint(indent + "  ")).append("\n");
+        sb.append(expressionList.prettyPrint(indent + "  ")).append("\n");
+        sb.append(selectStatment.prettyPrint(indent + "  ")).append("\n");
+        sb.append(exitPredicate.prettyPrint(indent + "  ")).append("\n");
+        sb.append(scalarSubquery.prettyPrint(indent + "  ")).append("\n");
+        for (ASTNode whenClauseChild : whenClauseChildren) {
+            sb.append(whenClauseChild.prettyPrint(indent + "  ")).append("\n");
+        }
+        return sb.toString().trim();
+    }
+}
