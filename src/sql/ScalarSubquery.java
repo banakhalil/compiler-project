15a69647f64aa153fa828c1b56cb456f5ee6ab65
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-20 10:59:54 +0300

    added the scalar subquery class

---
+++ src/sql/ScalarSubquery.java
@@ -0,0 +1,20 @@
+package sql;
+
+public class ScalarSubquery extends ASTNode{
+    private ASTNode subquery;
+    private ASTNode selectStatement;
+
+    public ScalarSubquery(ASTNode subquery, ASTNode selectStatement) {
+        this.subquery = subquery;
+        this.selectStatement = selectStatement;
+    }
+
+    @Override
+    public String prettyPrint(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent).append("SCALAR SUBQUERY:\n");
+        sb.append(subquery.prettyPrint(indent + "  ")).append("\n");
+        sb.append(selectStatement.prettyPrint(indent + "  ")).append("\n");
+        return sb.toString();
+    }
+}
