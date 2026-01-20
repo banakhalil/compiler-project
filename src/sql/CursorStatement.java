7977770bb30dd24ebaa36e905706f117d67252f3
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-20 12:07:43 +0300

    added the cursor statement

---
+++ src/sql/CursorStatement.java
@@ -0,0 +1,23 @@
+package sql;
+
+public class CursorStatement extends ASTNode {
+    private ASTNode declareCursor;
+    private ASTNode openCursor;
+    private ASTNode fetchStatement;
+
+    public CursorStatement(ASTNode declareCursor, ASTNode openCursor, ASTNode fetchStatement) {
+        this.declareCursor = declareCursor;
+        this.openCursor = openCursor;
+        this.fetchStatement = fetchStatement;
+    }
+
+    @Override
+    public String prettyPrint(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent).append("CURSOR STATEMENT:\n");
+        sb.append(declareCursor.prettyPrint(indent + "  ")).append("\n");
+        sb.append(openCursor.prettyPrint(indent + "  ")).append("\n");
+        sb.append(fetchStatement.prettyPrint(indent + "  ")).append("\n");
+        return sb.toString().trim();
+    }
+}
