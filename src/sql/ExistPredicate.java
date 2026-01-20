4c5d0cdd3dc4449ebf3e39e5a245ce5de1200fed
Batoul Allahham <batoolallahham5@gmail.com>
2026-01-20 11:09:28 +0300

    added the exist predicate class

---
+++ src/sql/ExistPredicate.java
@@ -0,0 +1,17 @@
+package sql;
+
+public class ExistPredicate extends ASTNode{
+    private ASTNode subquery;
+
+    public ExistPredicate(ASTNode subquery) {
+        this.subquery = subquery;
+    }
+
+    @Override
+    public String prettyPrint(String indent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(indent).append("EXIST PREDICATE:\n");
+        sb.append(subquery.prettyPrint(indent + "  ")).append("\n");
+        return sb.toString();
+    }
+}
